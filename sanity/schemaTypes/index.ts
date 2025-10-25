415271e00be71096e2fdd9d116a6f4d19b2edfea
Joshua Carroll <carrolljosh1976@gmail.com>
2025-10-25 17:25:34 -0400

    added acheivement, blog, certification, contact, education, experience, navigation, project,service, siteSettings, skill,and testimonial schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,4 +1,17 @@
 import { type SchemaTypeDefinition } from "sanity";
+import profile from "./profile";
+import project from "./project";
+import skill from "./skill";
+import experience from "./experience";
+import education from "./education";
+import testimonial from "./testimonial";
+import certification from "./certification";
+import achievement from "./achievement";
+import blog from "./blog";
+import service from "./service";
+import contact from "./contact";
+import siteSettings from "./siteSettings";
+import navigation from "./navigation";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
